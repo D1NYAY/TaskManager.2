b841697bbc701d6f6bb5b80d7a504ab9455417d1
D1NYAY <otorbaev.bro26021@gmail.com>
2022-10-23 22:10:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskmanager/data/Task.kt
@@ -0,0 +1,9 @@
+package com.example.taskmanager.data
+
+import java.io.FileDescriptor
+
+data class Task(
+    var title:String? = null,
+    var description:String? = null,
+    var time:Long? = null,
+)
---
+++ app/src/main/java/com/example/taskmanager/ui/task/TaskFragment.kt
@@ -0,0 +1,26 @@
+package com.example.taskmanager.ui.task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmanager.databinding.FragmentTaskBinding
+
+class TaskFragment : Fragment() {
+    private lateinit var binding : FragmentTaskBinding
+
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentTaskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
